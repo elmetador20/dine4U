748e182071d8dbf2cd8018f0a9e3c4fc0e178b6b
sharique ahmad <ahmedsharique250@gmail.com>
2025-09-22 02:35:36 +0530

    project setup

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+
+
+export default function Index() {
+  return (
+      <View className="flex-1 items-center justify-center bg-orange-200">
+      <Text className="text-xl font-bold text-red-800">
+        Welcome to Nativewind!
+      </Text>
+    </View>
+  );
+}
