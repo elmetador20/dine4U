a27881b71763b35916d6e772df3f1c6a98d97702
sharique ahmad <ahmedsharique250@gmail.com>
2025-09-24 14:20:34 +0530

    changes

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,17 @@
-import { Text, View } from "react-native";
+import { FlatList, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 
 
 export default function Index() {
   return (
-      <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl text-primary text-center font-bold  font-quicksand-light">
-        Welcome to my react native app!
-      </Text>
-    </View>
+      <SafeAreaView>
+       <FlatList
+        // data={[]}
+        // renderItem={({items})}
+        />
+       
+  
+    </SafeAreaView>
   );
 }
