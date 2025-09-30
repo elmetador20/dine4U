485bec53693eec5bb94654f7dd4251643c5bc006
sharique ahmad <ahmedsharique250@gmail.com>
2025-09-30 18:53:08 +0530

    added appwrite

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -3,5 +3,7 @@ import { Platform } from "react-native";
 export const appwriteConfig={
   endpoint:process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
   Platform:"com.dine4u.foodorderning",
-  projectId:process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID
+  projectId:process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  databaseId:'68d7c1b30025986a03de',
+  userCollectionId:'user'
 }
